0e1549f858fa2be62d3314ad4cd626da6bc14ef2
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2017-09-05 16:20:56 +0530

    Create sanjeevi.c

---
+++ sanjeevi.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n<=0)
+{
+if(n==0)
+printf("it is zero");
+else
+printf("it is negative");
+}
+else
+{
+printf("it is passitive");
+}
+}
